3e825e6329412587a023a4ae5ae413fe25d0fc4c
DevNerdAK1111 <mike.helms@outlook.de>
2026-08-04 12:14:02 +0200

    Update constants.py

--- backend/constants.py
+++ backend/constants.py
@@ -1,1 +1,39 @@
+# -----------------------------------------------------------------------------
+# CONSTANTS & STRATEGIES
+# -----------------------------------------------------------------------------
 
+GRUNDERWERBSTEUER_MAP = {
+    "Baden-Württemberg": 0.050, "Bayern": 0.035, "Berlin": 0.060,
+    "Brandenburg": 0.065, "Bremen": 0.050, "Hamburg": 0.055,
+    "Hessen": 0.060, "Mecklenburg-Vorpommern": 0.060, "Niedersachsen": 0.050,
+    "Nordrhein-Westfalen": 0.065, "Rheinland-Pfalz": 0.050, "Saarland": 0.065,
+    "Sachsen": 0.055, "Sachsen-Anhalt": 0.050, "Schleswig-Holstein": 0.065, "Thüringen": 0.065
+}
+
+OBJEKTARTEN = [
+    "Eigentumswohnung", "Einfamilienhaus", "Zweifamilienhaus",
+    "Reihenhaus / Doppelhaushälfte", "Mehrfamilienhaus",
+    "Wohn- und Geschäftshaus", "Mikroapartment / Studentisches Wohnen",
+    "Pflege- / Seniorenimmobilie", "Gewerbeimmobilie / Sonstiges"
+]
+
+STRATEGIES = {
+    "Konservativ / Ausgewogen (Standard)": {
+        "target_cf": 50.0, "tol_cf": 0.0,
+        "target_rendite": 4.5, "tol_rendite": 3.8,
+        "target_roe": 8.0, "tol_roe": 4.0,
+        "target_dscr": 1.20, "tol_dscr": 1.05
+    },
+    "Cashflow-Fokus (B/C-Lage)": {
+        "target_cf": 150.0, "tol_cf": 50.0,
+        "target_rendite": 6.0, "tol_rendite": 5.0,
+        "target_roe": 12.0, "tol_roe": 7.0,
+        "target_dscr": 1.25, "tol_dscr": 1.10
+    },
+    "Wertwachstum / A-Lage": {
+        "target_cf": 0.0, "tol_cf": -50.0,
+        "target_rendite": 3.5, "tol_rendite": 2.8,
+        "target_roe": 6.0, "tol_roe": 3.0,
+        "target_dscr": 1.15, "tol_dscr": 1.05
+    }
+}

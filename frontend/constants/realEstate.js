4c0ee39a08557778c7390751127b02d5179e6112
DevNerdAK1111 <mike.helms@outlook.de>
2026-08-06 09:53:02 +0200

    Create realEstate.js

---
+++ frontend/constants/realEstate.js
@@ -0,0 +1,18 @@
+export const BUNDESLAENDER_DEFAULT = {
+  'Baden-Württemberg': 5.0,
+  'Bayern': 3.5,
+  'Berlin': 6.0,
+  'Brandenburg': 6.5,
+  'Bremen': 5.0,
+  'Hamburg': 5.5,
+  'Hessen': 6.0,
+  'Mecklenburg-Vorpommern': 6.0,
+  'Niedersachsen': 5.0,
+  'Nordrhein-Westfalen': 6.5,
+  'Rheinland-Pfalz': 5.0,
+  'Saarland': 6.5,
+  'Sachsen': 5.5,
+  'Sachsen-Anhalt': 5.0,
+  'Schleswig-Holstein': 6.5,
+  'Thüringen': 6.5
+};
